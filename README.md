5ff2cb262029eeb6fbccb1451b89b396d180bf34
oberrich <oberrich@img.ws>
2025-01-04 20:03:29 +0100

    initial

---
+++ README.md
@@ -0,0 +1,99 @@
+## `doxygen-bindgen` Crate
+
+The `doxygen-bindgen` crate transforms Doxygen-style comments into Rustdoc-style markdown documentation. It helps convert Doxygen annotations used in C/C++ code into the appropriate Rustdoc format, making it easier to migrate or document codebases in Rust.
+
+### Usage
+
+```toml
+[build-dependencies]
+doxygen-bindgen = "0.1"
+```
+
+```rust
+use bindgen::callbacks::ParseCallbacks;
+
+#[derive(Debug)]
+struct ProcessComments;
+
+impl ParseCallbacks for ProcessComments {
+    fn process_comment(&self, comment: &str) -> Option<String> {
+        match crate::doxygen::transform(comment) {
+            Ok(res) => Some(res),
+            Err(err) => {
+                println!("cargo:warning=Problem processing doxygen comment: {comment}\n{err}");
+                None
+            }
+        }
+    }
+}
+
+bindgen::builder()
+   .parse_callbacks(Box::new(ProcessComments))
+```
+
+### Example
+
+```
+/**
+ * Creates a new registry key or opens an existing one, and it associates the key with a transaction.
+ * 
+ * @param[out] KeyHandle A pointer to a handle that receives the key handle.
+ * @param[in] DesiredAccess The access mask that specifies the desired access rights.
+ * @param[in] ObjectAttributes A pointer to an OBJECT_ATTRIBUTES structure that specifies the object attributes.
+ * @param[in] TitleIndex Reserved.
+ * @param[in, optional] Class A pointer to a UNICODE_STRING structure that specifies the class of the key.
+ * @param[in] CreateOptions The options to use when creating the key.
+ * @param[in] TransactionHandle A handle to the transaction.
+ * @param[out, optional] Disposition A pointer to a variable that receives the disposition value.
+ * @return NTSTATUS Successful or errant status.
+ */
+ ```
+
+ Doxygen markup for [NtCreateKeyTransacted](https://docs.rs/phnt/latest/phnt/ffi/fn.NtCreateKeyTransacted.html) is transformed into:
+
+```markdown
+Creates a new registry key or opens an existing one, and it associates the key with a transaction.
+# Arguments
+
+* `KeyHandle` [out]  - A pointer to a handle that receives the key handle.
+* `DesiredAccess` [in]  - The access mask that specifies the desired access rights.
+* `ObjectAttributes` [in]  - A pointer to an OBJECT_ATTRIBUTES structure that specifies the object attributes.
+* `TitleIndex` [in]  - Reserved.\n* `Class` [in, optional]  - A pointer to a UNICODE_STRING structure that specifies the class of the key.
+* `CreateOptions` [in]  - The options to use when creating the key.
+* `TransactionHandle` [in]  - A handle to the transaction.
+* `Disposition` [out, optional]  - A pointer to a variable that receives the disposition value.
+# Returns
+
+NTSTATUS Successful or errant status.
+```
+
+
+### Available Doxygen commands
+
+| **Doxygen**                   | **Markdown**                  |
+|-------------------------------|-------------------------------|
+| `brief`, `short`              |                               |
+| `param`                       | ``# Arguments\n\n* `name` -`` |
+| `see`, `sa`                   | ``# See also\n\n> [`ref`]``   |
+| `ref`                         | ``[`ref`]``                   |
+| `a`, `e`, `em`                | _word_                        |
+| `b`                           | **word**                      |
+| `c`, `p`                      | `word`                        |
+| `note`                        | `> **Note** `                 |
+| `since`                       | `> **Since** `                |
+| `deprecated`                  | `> **Deprecated** `           |
+| `remark`, `remarks`           | `> `                          |
+| `li`                          | `- `                          |
+| `par`                         | `# `                          |
+| `returns`, `return`, `result` | ``# Returns\n\n``             |
+| `{`, `}`                      | Not implemented               |
+
+Here’s the updated README with the **Usage** section included and a note about the updated `transform` function that no longer panics:
+
+### License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+### Contribution
+
+We welcome contributions! If you'd like to help improve this crate, feel free to submit a pull request or open an issue.
---
+++ src/lib.rs
@@ -0,0 +1,115 @@
+use std::error::Error;
+use yap::{IntoTokens, Tokens};
+
+static SEPS: [char; 5] = [' ', '\t', '\r', '\n', '['];
+
+/// Formats a reference string as markdown.
+pub fn format_ref(str: String) -> String {
+    assert!(!str.contains(' '));
+    if !str.contains("://") {
+        format!("[`{}`]", str)
+    } else {
+        str
+    }
+}
+
+/// Extracts the next word token.
+fn take_word(toks: &mut impl Tokens<Item = char>) -> String {
+    toks.take_while(|&c| !SEPS.into_iter().any(|s| c == s))
+        .collect::<String>()
+}
+
+/// Skips whitespace tokens.
+fn skip_whitespace(toks: &mut impl Tokens<Item = char>) {
+    toks.skip_while(|c| c.is_ascii_whitespace());
+}
+
+/// Emits a section header if it's not already emitted.
+fn emit_section_header(output: &mut Vec<String>, header: &str) {
+    if !output.iter().any(|line| line.trim() == header) {
+        output.push(header.to_owned());
+        output.push("\n\n".to_owned());
+    }
+}
+
+/// Transforms Doxygen comments into markdown for Rustdoc.
+pub fn transform(str: &str) -> Result<String, Box<dyn Error>> {
+    let mut res: Vec<String> = vec![];
+    let mut toks = str.into_tokens();
+
+    skip_whitespace(&mut toks);
+    while let Some(tok) = toks.next() {
+        if "@\\".chars().any(|c| c == tok) {
+            let tag = take_word(&mut toks);
+            skip_whitespace(&mut toks);
+            match tag.as_str() {
+                "param" => {
+                    emit_section_header(&mut res, "# Arguments");
+                    let (mut argument, mut attributes) = (take_word(&mut toks), "".to_owned());
+                    if argument.is_empty() {
+                        if toks.next() != Some('[') {
+                            return Err("Expected opening '[' inside attribute list".into());
+                        }
+                        attributes = toks.take_while(|&c| c != ']').collect::<String>();
+                        if toks.next() != Some(']') {
+                            return Err("Expected closing ']' inside attribute list".into());
+                        }
+                        attributes = format!(" [{}] ", attributes);
+                        skip_whitespace(&mut toks);
+                        argument = take_word(&mut toks);
+                    }
+                    res.push(format!("* `{}`{} -", argument, attributes));
+                }
+                "c" | "p" => res.push(format!("`{}`", take_word(&mut toks))),
+                "ref" => res.push(format_ref(take_word(&mut toks))),
+                "see" | "sa" => {
+                    emit_section_header(&mut res, "# See also");
+                    res.push(format!("> {}", format_ref(take_word(&mut toks))));
+                }
+                "a" | "e" | "em" => res.push(format!("_{}_", take_word(&mut toks))),
+                "b" => res.push(format!("**{}**", take_word(&mut toks))),
+                "note" => res.push("> **Note** ".to_owned()),
+                "since" => res.push("> **Since** ".to_owned()),
+                "deprecated" => res.push("> **Deprecated** ".to_owned()),
+                "remark" | "remarks" => res.push("> ".to_owned()),
+                "li" => res.push("- ".to_owned()),
+                "par" => res.push("# ".to_owned()),
+                "returns" | "return" | "result" => emit_section_header(&mut res, "# Returns"),
+                "{" => { /* group start, not implemented  */ }
+                "}" => { /* group end, not implemented */ }
+                "brief" | "short" => {}
+                _ => res.push(format!("{tok}{tag} ")),
+            }
+        } else if tok == '\n' {
+            skip_whitespace(&mut toks);
+            res.push(format!("{tok}"));
+        } else {
+            res.push(format!("{tok}"));
+        }
+    }
+    Ok(res.join(""))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basic() {
+        const S: &str = "The FILE_BASIC_INFORMATION structure contains timestamps and basic attributes of a file.\n \\li If you specify a value of zero for any of the XxxTime members, the file system keeps a file's current value for that time.\n \\li If you specify a value of -1 for any of the XxxTime members, time stamp updates are disabled for I/O operations preformed on the file handle.\n\\li If you specify a value of -2 for any of the XxxTime members, time stamp updates are enabled for I/O operations preformed on the file handle.\n\\remarks To set the members of this structure, the caller must have FILE_WRITE_ATTRIBUTES access to the file.";
+        const S_: &str = "The FILE_BASIC_INFORMATION structure contains timestamps and basic attributes of a file.\n- If you specify a value of zero for any of the XxxTime members, the file system keeps a file's current value for that time.\n- If you specify a value of -1 for any of the XxxTime members, time stamp updates are disabled for I/O operations preformed on the file handle.\n- If you specify a value of -2 for any of the XxxTime members, time stamp updates are enabled for I/O operations preformed on the file handle.\n> To set the members of this structure, the caller must have FILE_WRITE_ATTRIBUTES access to the file.";
+        assert_eq!(crate::transform(S).unwrap(), S_);
+    }
+
+    #[test]
+    fn with_sections() {
+        const S: &str = " The NtDelayExecution routine suspends the current thread until the specified condition is met.\n\n @param Alertable The function returns when either the time-out period has elapsed or when the APC function is called.\n @param DelayInterval The time interval for which execution is to be suspended, in milliseconds.\n - A value of zero causes the thread to relinquish the remainder of its time slice to any other thread that is ready to run.\n - If there are no other threads ready to run, the function returns immediately, and the thread continues execution.\n - A value of INFINITE indicates that the suspension should not time out.\n @return NTSTATUS Successful or errant status. The return value is STATUS_USER_APC when Alertable is TRUE, and the function returned due to one or more I/O completion callback functions.\n @remarks Note that a ready thread is not guaranteed to run immediately. Consequently, the thread will not run until some arbitrary time after the sleep interval elapses,\n based upon the system \"tick\" frequency and the load factor from other processes.\n @see https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-sleepex";
+        const S_: &str = "The NtDelayExecution routine suspends the current thread until the specified condition is met.\n# Arguments\n\n* `Alertable` - The function returns when either the time-out period has elapsed or when the APC function is called.\n* `DelayInterval` - The time interval for which execution is to be suspended, in milliseconds.\n- A value of zero causes the thread to relinquish the remainder of its time slice to any other thread that is ready to run.\n- If there are no other threads ready to run, the function returns immediately, and the thread continues execution.\n- A value of INFINITE indicates that the suspension should not time out.\n# Returns\n\nNTSTATUS Successful or errant status. The return value is STATUS_USER_APC when Alertable is TRUE, and the function returned due to one or more I/O completion callback functions.\n> Note that a ready thread is not guaranteed to run immediately. Consequently, the thread will not run until some arbitrary time after the sleep interval elapses,\nbased upon the system \"tick\" frequency and the load factor from other processes.\n# See also\n\n> https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-sleepex";
+        assert_eq!(crate::transform(S).unwrap(), S_);
+    }
+
+    #[test]
+    fn with_attributes() {
+        const S: &str = "Creates a new registry key or opens an existing one, and it associates the key with a transaction.\n\n@param[out] KeyHandle A pointer to a handle that receives the key handle.\n @param[in] DesiredAccess The access mask that specifies the desired access rights.\n@param[in] ObjectAttributes A pointer to an OBJECT_ATTRIBUTES structure that specifies the object attributes.\n@param[in] TitleIndex Reserved.\n@param[in, optional] Class A pointer to a UNICODE_STRING structure that specifies the class of the key.\n @param[in] CreateOptions The options to use when creating the key.\n@param[in] TransactionHandle A handle to the transaction.\n @param[out, optional] Disposition A pointer to a variable that receives the disposition value.\n@return NTSTATUS Successful or errant status.\n";
+        const S_: &str = "Creates a new registry key or opens an existing one, and it associates the key with a transaction.\n# Arguments\n\n* `KeyHandle` [out]  - A pointer to a handle that receives the key handle.\n* `DesiredAccess` [in]  - The access mask that specifies the desired access rights.\n* `ObjectAttributes` [in]  - A pointer to an OBJECT_ATTRIBUTES structure that specifies the object attributes.\n* `TitleIndex` [in]  - Reserved.\n* `Class` [in, optional]  - A pointer to a UNICODE_STRING structure that specifies the class of the key.\n* `CreateOptions` [in]  - The options to use when creating the key.\n* `TransactionHandle` [in]  - A handle to the transaction.\n* `Disposition` [out, optional]  - A pointer to a variable that receives the disposition value.\n# Returns\n\nNTSTATUS Successful or errant status.\n";
+        assert_eq!(crate::transform(S).unwrap(), S_);
+    }
+}
